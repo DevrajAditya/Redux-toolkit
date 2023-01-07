b63d1daafa5c4d800d73dbe23a4b1832f6acdb52
Aditya <jhaaditya0101@gmail.com>
2023-01-08 00:28:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import store from './store/store';
+import Navbar from './components/Navbar';
+import { Provider } from 'react-redux';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Provider store={store}>
+     <BrowserRouter>
+     <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}>Home</Route>
+        <Route path="/cart" element={<Cart />}>Cart</Route>
+      </Routes>
+     </BrowserRouter>
+     </Provider>
     </div>
   );
 }
